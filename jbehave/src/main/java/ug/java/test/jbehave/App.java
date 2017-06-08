495f17ef2f197951bb9241bd504830e8c0382225
AliaksandrN <novik.alexander.88@gmail.com>
2017-06-08 15:43:41 +0200

    test

---
+++ jbehave/src/main/java/ug/java/test/jbehave/App.java
@@ -0,0 +1,13 @@
+package ug.java.test.jbehave;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
